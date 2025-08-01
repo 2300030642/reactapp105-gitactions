7dd5cc05c3010e05c92538ea8af65423b562fac8
Nishitha <nishithanishitha3434@gmail.com>
2025-08-01 23:51:41 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ function App() {
       <h1>Welcome to My React App Project - S105</h1>
       <h1>CI/CD and Cloud Devops Course</h1>
       <p>This is a sample homepage deployed on GitHub Actions using Vite.</p>
+      <h1>Done with Git Pages and Actions</h1>
     </div>
   )
 }
