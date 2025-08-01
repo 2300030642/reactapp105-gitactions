27c4ab6b72b780341a9bb9fc6dae01d0fce26c50
Nishitha <nishithanishitha3434@gmail.com>
2025-08-01 22:55:01 +0530

    react project v1.0

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Welcome to My React App Project - S105</h1>
+      <p>This is a sample homepage deployed on GitHub Pages using Vite.</p>
+    </div>
+  )
+}
+
+export default App
